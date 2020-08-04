e1501995815852f4efa9ab0165fbb5c71e4c2869
poojagaikwad1358 <poojagaikwad1358@gmail.com>
2020-08-04 22:54:09 +0530

    Added: Function to reset board.

---
+++ ticTacToe.sh
@@ -0,0 +1,15 @@
+#!/bin/bash -x
+
+#declare dictionary array
+declare -a positions
+
+#Function to reset board
+function resetBoard()
+{
+	for (( i=1; i<=9; i++ ))
+	do
+		positions[$i]=$i
+	done
+	echo "Board reset successful."
+}
+resetBoard
