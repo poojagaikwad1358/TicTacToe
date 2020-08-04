8e8d1aef1221c7a24aad977d1ed5b564108a53e3
poojagaikwad1358 <poojagaikwad1358@gmail.com>
2020-08-04 23:21:23 +0530

    Added: Assign symbol to computer and user

--- ticTacToe.sh
+++ ticTacToe.sh
@@ -14,15 +14,38 @@ function resetBoard()
 }
 
 #function toss to check who win toss
+#Assign X & O symbols to computer and user.
 function toss()
 {
 	random=$(( RANDOM % 2 ))
 	if [ $random -eq 1 ]
 	then
 		echo "Computer won toss."
+
+		symbol=$(( RANDOM % 2 ))
+		if [ $symbol -eq 1 ]
+		then
+			computerSymbol='X'
+			userSymbol='O'
+		else
+			computerSymbol='O'
+			userSymbol='X'
+		fi
 	else
 		echo "You won toss."
+
+		read -p "Enter 1 for 'X' and 2 for 'O': " symbol
+		if [ $symbol -eq 1 ]
+		then
+			userSymbol='X'
+			computerSymbol='O'
+		else
+			userSymbol='O'
+			computerSymbol='X'
+		fi
 	fi
+	echo "Computer: $computerSymbol"
+	echo "User: $userSymbol"
 }
 
 resetBoard
